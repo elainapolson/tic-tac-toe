a925a5fd981ae2e833d0dc23602076988238ffe0
elainapolson <elainapolson@gmail.com>
2015-08-24 21:15:52 -0400

    refactor human player class to add input validator

--- human_player.rb
+++ human_player.rb
@@ -3,17 +3,17 @@ class HumanPlayer
   attr_accessor :name, :symbol
 
   def make_move(board, input)
-    if !(input >= 1 && input <= 9)
-      puts "Ummmm, that doesn't look like a number between 1 - 9. Let's try that again." 
-      input = gets.chomp.to_i
-      make_move(board, input)
-    elsif !(board.available_cells.include?(input))
-      puts "Do you need glasses?? That spot is taken! Pick another spot." 
+    if invalid_input(board, input)
+      puts "Invalid input! Enter a valid space." 
       input = gets.chomp.to_i
       make_move(board, input)
     else 
       board.cells[input][:value] = self.symbol
     end
   end 
+
+  def invalid_input(board, input)
+    !(input >= 1 && input <= 9) || !(board.available_cells.include?(input))
+  end
 
 end
